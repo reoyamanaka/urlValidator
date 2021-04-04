712b64ce05794ea82c4f38b085848bc7c0baaa92
Reo Yamanaka <ryamanaka807@gmail.com>
2021-04-04 17:11:34 -0400

    added error throw

--- frontendValidation.js
+++ frontendValidation.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+    $("urlSubmit").click(function(){
+        var url = $("formUrl").val();
+
+    });
+
+
     $("form").submit(function(event) {
         event.preventDefault();
         var url = $("#formUrl").val();
+        if (url.substring(0, 8) != "https://" && url.substring(0, 7) != "http://" && url !== "") {
+            alert("You forgot to include a http:// or a https://");
+            throw new Error("You forgot to include a http:// or a https://");
+        }
         var submit = $("#urlSubmit").val();
         $(".formMessage").load("backendValidation.php", {
             url: url,
